f59520ed7245aacdfc79d1f4ffb3d599d783359c
Mike Williamson <mike@korora.ca>
2024-06-09 16:20:33 -0400

    Update translations

--- src/locales/fr/messages.js
+++ src/locales/fr/messages.js
@@ -1,5 +1,1 @@
-/*eslint-disable*/ module.exports = {
-  messages: JSON.parse(
-    '{"mqdkdN":"Agence de la santé<0/>publique du Canada","rdjv6Q":"Certains contenus de test."}',
-  ),
-}
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"3ZRb9C\":\"Le drapeau canadien\",\"mqdkdN\":\"Agence de la santé<0/>publique du Canada\",\"rdjv6Q\":\"Certains contenus de test.\"}")};
