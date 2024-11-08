25a9623f897035bb93636076fe6e6358365eca94
Mike Williamson <mike@korora.ca>
2024-11-08 16:31:56 -0500

    Update translation files

--- src/locales/en/messages.js
+++ src/locales/en/messages.js
@@ -1,5 +1,1 @@
-/*eslint-disable*/ module.exports = {
-  messages: JSON.parse(
-    '{"3ZRb9C":"Canadian Flag","mqdkdN":"Public Health<0/>Agency of Canada","rdjv6Q":"Some test content."}',
-  ),
-}
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"3ZRb9C\":\"Canadian Flag\",\"mqdkdN\":\"Public Health<0/>Agency of Canada\",\"rdjv6Q\":\"Some test content.\",\"eyFYak\":\"Symbol of the Government of Canada\"}")};
